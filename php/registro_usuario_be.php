66edb5b7ef73532863d0ff7bba2fc3e1abd65807
mario <mario.gonzalez@hotmail.com.ar>
2021-11-23 00:39:27 -0300

    js

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -3,14 +3,6 @@
 
 include 'conexion_be.php';//conexion a base de datos
 
-/*
-if ($conexion) {//Si la conexion es exitosa
-        echo "Conexion exitosa !!!";//Mensaje de conexion exitosa
-    }else{//Si la conexion no es exitosa
-        echo "Conexion fallida";//Mensaje de conexion fallida
-  */  
-
-
 $nombre=$_POST['nombre'];//recibimos el nombre
 $apellido=$_POST['apellido'];//recibimos el apellido
 $email=$_POST['email'];//recibimos el email
@@ -19,12 +11,9 @@ $pass2=$_POST['clave2'];//recibimos la clave
 
 
 //$insertar="INSERT INTO usuario (nombre, apellido, email, clave) 
-$insertar="INSERT INTO`usuario`(`usuario`,`clave`)VALUES (NULL,'$nombre','$pass1','1','1')";//insertamos los datos en la tabla usuarios
-
+$insertar="INSERT INTO`usuario`(`id_usuario`,`usuario`,`clave`, `cliente`, `estado`)
+           VALUES (NULL,'$nombre','$pass1','1','1')";//insertamos los datos en la tabla usuarios
 
 $ejecutar = mysqli_query($conexion, $insertar);//conectamos y ejecutamos la consulta
-
-
-
 
 ?>
