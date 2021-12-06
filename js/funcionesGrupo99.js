3c4629d26d9df9cecc38d5bdc71416c2e4b96f69
mario <mario.gonzalez@hotmail.com.ar>
2021-12-06 16:44:41 -0300

    js

--- js/funcionesGrupo99.js
+++ js/funcionesGrupo99.js
@@ -1,9 +1,8 @@
 
 //-----------------------------------------------------------------------------------
-//primera funcion DOM del sitio trabaja sobre el NAVBAR colocando fecha y hora actual
+//funcion DOM del sitio trabaja sobre el NAVBAR colocando fecha y hora actual
 //-----------------------------------------------------------------------------------
 showTime();
-
 function showTime(){
 fecha = new Date();//instancio objeto Date con fecha 
 hora = fecha.getHours();//obtengo hora actual
@@ -25,8 +24,7 @@ setTimeout("showTime()", 1000);//llamo a la funcion showTime cada segundo para s
 //-----------------------------------------------------------------------------------
 //funcion DOM del sitio trabaja sobre el NAVBAR expandiendo o colapsando menu
 //-----------------------------------------------------------------------------------
-function mi_Menu() {
-  //const navToggle = document.querySelector(".nav-toggle"); //obtengo el elemento nav-toggle
+function mi_Menu() {//funcion para expandir o colapsar menu
   const navMenu = document.querySelector(".nav-menu"); //obtengo el elemento nav-menu
   navMenu.classList.toggle("nav-menu__visible");
   //si el elemento nav-menu tiene la clase nav-menu__visible la quito y si no la agrego
@@ -38,24 +36,24 @@ function mi_Menu() {
 
 
 //-----------------------------------------------------------------------------------
-//Envio por metodo get a otro formulario cointacto a otro formulario
+//Envio por metodo get a formulario contacto1 los valores cargados en contacto
 //-----------------------------------------------------------------------------------
-function mi_Envio(url, ubicacion) {
-  document.getElementById("5").value =
-    "°" +
-    document.getElementById("1").value +
-    "°" +
-    document.getElementById("2").value +
-    "°" +
-    document.getElementById("3").value +
-    "°" +
-    document.getElementById("4").value +
-    "°";
+function mi_Envio(url, ubicacion) {//funcion para enviar los datos del formulario
+  document.getElementById("5").value =//cargo en input 5 hidden el valos de los 4 inputs
+    "°" +//declaro una bandera para saber a donde empieza input
+    document.getElementById("1").value +//cargo en input 5 hidden el valor del input 1
+    "°" +//declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("2").value +//cargo en input 5 hidden el valor del input 2
+    "°" +//declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("3").value +//cargo en input 5 hidden el valor del input 3
+    "°" +//declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("4").value +//cargo en input 5 hidden el valor del input 4
+    "°";//declaro una bandera para saber a donde termina cada input
   window.open(url + '?payload=' + document.getElementById('5').value, ubicacion, 'width=870,height=700'); // return false;
   direccionar();
 }
 
-function direccionar() {
+function direccionar() {//funcion para direccionar a la pagina de contacto1
   window.location("");
 }
 
@@ -72,60 +70,74 @@ function direccionar() {
 //Envio por metodo get a otro formulario cointacto a otro formulario
 //-----------------------------------------------------------------------------------
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //-----------------------------------------------------------------------------------
 //funcion DOM filtra componentes de la lista
 //-----------------------------------------------------------------------------------
 function borra() {//funcion para borrar los componentes que se muestran
   var x, i;
-  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");//obtengo todos los componentes
+  for (i = 0; i < x.length; i++) {//recorro todos los componentes
+   x[i].style.display = "none";//oculto los componentes
   }
 }
 function todo() {//funcion para mostrar todos los componentes
   var x, i;
-  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");//obtengo todos los componentes
+  for (i = 0; i < x.length; i++) {//recorro todos los componentes
+    x[i].style.display = "flex";//muestro los componentes
   }
 }
 function leds() {//funcion para mostrar los componentes led
-  borra();
+  borra();//borro los componentes
   var x, i;
-  x = document.querySelectorAll(".led");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".led");//obtengo los componentes led
+  for (i = 0; i < x.length; i++) {//recorro los componentes 
+    x[i].style.display = "flex";//muestro los componentes led
   }
 }
 function disp() {//funcion para mostrar los componentes display
-  borra();
+  borra();//borro los componentes
   var x, i;
-  x = document.querySelectorAll(".dis");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".dis");//obtengo los componentes display
+  for (i = 0; i < x.length; i++) {//recorro los componentes
+    x[i].style.display = "flex";//muestro los componentes display
   }
 }
 function tran() {//funcion para mostrar los componentes transistores
-  borra();
+  borra();//borro los componentes
   var x, i;
-  x = document.querySelectorAll(".tra");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".tra");//obtengo los componentes transistores
+  for (i = 0; i < x.length; i++) {//recorro los componentes
+    x[i].style.display = "flex";//muestro los componentes transistores
   }
 }
 function serv() {//funcion para mostrar los componentes transistores
-  borra();
+  borra();//borro los componentes
   var x, i;
-  x = document.querySelectorAll(".ser");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".ser");//obtengo los componentes servidores
+  for (i = 0; i < x.length; i++) {//recorro los componentes
+    x[i].style.display = "flex";//muestro los componentes servidores
   }
 }
 function gral() {//funcion para mostrar los componentes transistores
-  borra();
+  borra();//borro los componentes
   var x, i;
-  x = document.querySelectorAll(".gra");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "flex";
+  x = document.querySelectorAll(".gra");//obtengo los componentes generales
+  for (i = 0; i < x.length; i++) {//recorro los componentes
+    x[i].style.display = "flex";//muestro los componentes generales
   }
 }
