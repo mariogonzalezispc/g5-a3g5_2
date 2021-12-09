e68ceea1134c7f44f3b6ebd823a203c28b8d3627
Mario <mario.gonzalez@hotmail.com.ar>
2021-12-09 16:21:37 -0300

    js

--- js/funcionesGrupo99.js
+++ js/funcionesGrupo99.js
@@ -1,35 +1,36 @@
-
 //-----------------------------------------------------------------------------------
 //funcion DOM del sitio trabaja sobre el NAVBAR colocando fecha y hora actual
 //-----------------------------------------------------------------------------------
 showTime();
-function showTime(){
-fecha = new Date();//instancio objeto Date con fecha 
-hora = fecha.getHours();//obtengo hora actual
-minutos = fecha.getMinutes();//obtengo minutos actual
-segundos = fecha.getSeconds();//obtengo segundos actual
-dia = fecha.getDate();//obtengo dia actual
-mes = fecha.getMonth()+1;//obtengo mes actual
-anio = fecha.getFullYear();//obtengo año actual
 
-if (hora < 10) hora = 0 + hora;//si hora es menor a 10 le agrego un 0 al inicio
-if (minutos < 10) minutos = "0" + minutos;//si minutos es menor a 10 le agrego un 0 al inicio
-if (segundos < 10) segundos = "0" + segundos;//si segundos es menor a 10 le agrego un 0 al inicio
+function showTime() {
+  fecha = new Date(); //instancio objeto Date con fecha 
+  hora = fecha.getHours(); //obtengo hora actual
+  minutos = fecha.getMinutes(); //obtengo minutos actual
+  segundos = fecha.getSeconds(); //obtengo segundos actual
+  dia = fecha.getDate(); //obtengo dia actual
+  mes = fecha.getMonth() + 1; //obtengo mes actual
+  anio = fecha.getFullYear(); //obtengo año actual
 
-$("#hora").text(hora+ ":" +minutos+ ":" +segundos);//muestro hora actual en el h1 id hora
-$("#dia").text(dia+ "/" +mes+ "/" +anio);//muestro dia actual en el h1 id dia
-setTimeout("showTime()", 1000);//llamo a la funcion showTime cada segundo para ser tiempo real
+  if (hora < 10) hora = 0 + hora; //si hora es menor a 10 le agrego un 0 al inicio
+  if (minutos < 10) minutos = "0" + minutos; //si minutos es menor a 10 le agrego un 0 al inicio
+  if (segundos < 10) segundos = "0" + segundos; //si segundos es menor a 10 le agrego un 0 al inicio
+
+  $("#hora").text(hora + ":" + minutos + ":" + segundos); //muestro hora actual en el h1 id hora
+  $("#dia").text(dia + "/" + mes + "/" + anio); //muestro dia actual en el h1 id dia
+  setTimeout("showTime()", 1000); //llamo a la funcion showTime cada segundo para ser tiempo real
 }
 
 //-----------------------------------------------------------------------------------
 //funcion DOM del sitio trabaja sobre el NAVBAR expandiendo o colapsando menu
 //-----------------------------------------------------------------------------------
-function mi_Menu() {//funcion para expandir o colapsar menu
+function mi_Menu() { //funcion para expandir o colapsar menu
   const navMenu = document.querySelector(".nav-menu"); //obtengo el elemento nav-menu
   navMenu.classList.toggle("nav-menu__visible");
   //si el elemento nav-menu tiene la clase nav-menu__visible la quito y si no la agrego
 }
-function mi_Menu2() {//funcion para expandir o colapsar menu
+
+function mi_Menu2() { //funcion para expandir o colapsar menu
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
   //si el elemento nav-menu tiene la clase nav-menu__visible la quito y si no la agrego
@@ -39,91 +40,105 @@ function mi_Menu2() {//funcion para expandir o colapsar menu
 //-----------------------------------------------------------------------------------
 //Envio por metodo get a formulario contacto1 los valores cargados en contacto
 //-----------------------------------------------------------------------------------
-function mi_Envio(url, ubicacion) {//funcion para enviar los datos del formulario
-  document.getElementById("5").value =//cargo en input 5 hidden el valos de los 4 inputs
-    "°" +//declaro una bandera para saber a donde empieza input
-    document.getElementById("1").value +//cargo en input 5 hidden el valor del input 1
-    "°" +//declaro una bandera para saber a donde empieza y termina cada input
-    document.getElementById("2").value +//cargo en input 5 hidden el valor del input 2
-    "°" +//declaro una bandera para saber a donde empieza y termina cada input
-    document.getElementById("3").value +//cargo en input 5 hidden el valor del input 3
-    "°" +//declaro una bandera para saber a donde empieza y termina cada input
-    document.getElementById("4").value +//cargo en input 5 hidden el valor del input 4
-    "°";//declaro una bandera para saber a donde termina cada input
-  window.open(url + '?payload=' + document.getElementById('5').value, ubicacion, 'width=870,height=700'); // return false;
-  direccionar();
+function mi_Envio(url, ubicacion) { //funcion para enviar los datos del formulario
+  document.getElementById("5").value = //cargo en input 5 hidden el valos de los 4 inputs
+    "°" + //declaro una bandera para saber a donde empieza input
+    document.getElementById("nombres").value + //cargo en input 5 hidden el valor del input 1
+    "°" + //declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("email").value + //cargo en input 5 hidden el valor del input 2
+    "°" + //declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("telefono").value + //cargo en input 5 hidden el valor del input 3
+    "°" + //declaro una bandera para saber a donde empieza y termina cada input
+    document.getElementById("mensaje").value + //cargo en input 5 hidden el valor del input 4
+    "°"; //declaro una bandera para saber a donde termina cada input
+  window.open(url + '?payload=' + document.getElementById('5').value, ubicacion, 'width=870,height=660');
+  //voy_index(); //llamo a la funcion voy_index para cargar la pagina index
 }
-function direccionar() {//funcion para direccionar a la pagina de contacto1
-  window.location("");
-}
+
 
 
 //-----------------------------------------------------------------------------------
 //funcion DOM filtra componentes de la lista
 //-----------------------------------------------------------------------------------
-function borra() {//funcion para borrar los componentes que se muestran
+function borra() { //funcion para borrar los componentes que se muestran
   var x, i;
-  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");//obtengo todos los componentes
-  for (i = 0; i < x.length; i++) {//recorro todos los componentes
-   x[i].style.display = "none";//oculto los componentes
+  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra"); //obtengo todos los componentes
+  for (i = 0; i < x.length; i++) { //recorro todos los componentes
+    x[i].style.display = "none"; //oculto los componentes
   }
 }
-function todo() {//funcion para mostrar todos los componentes
+
+function todo() { //funcion para mostrar todos los componentes
   var x, i;
-  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra");//obtengo todos los componentes
-  for (i = 0; i < x.length; i++) {//recorro todos los componentes
-    x[i].style.display = "flex";//muestro los componentes
+  x = document.querySelectorAll(".led,.dis, .tra, .ser, .gra"); //obtengo todos los componentes
+  for (i = 0; i < x.length; i++) { //recorro todos los componentes
+    x[i].style.display = "flex"; //muestro los componentes
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
 }
-function leds() {//funcion para mostrar los componentes led
-  borra();//borro los componentes
+
+function leds() { //funcion para mostrar los componentes led
+  borra(); //borro los componentes
   var x, i;
-  x = document.querySelectorAll(".led");//obtengo los componentes led
-  for (i = 0; i < x.length; i++) {//recorro los componentes 
-    x[i].style.display = "flex";//muestro los componentes led
+  x = document.querySelectorAll(".led"); //obtengo los componentes led
+  for (i = 0; i < x.length; i++) { //recorro los componentes 
+    x[i].style.display = "flex"; //muestro los componentes led
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
 }
-function disp() {//funcion para mostrar los componentes display
-  borra();//borro los componentes
+
+function disp() { //funcion para mostrar los componentes display
+  borra(); //borro los componentes
   var x, i;
-  x = document.querySelectorAll(".dis");//obtengo los componentes display
-  for (i = 0; i < x.length; i++) {//recorro los componentes
-    x[i].style.display = "flex";//muestro los componentes display
+  x = document.querySelectorAll(".dis"); //obtengo los componentes display
+  for (i = 0; i < x.length; i++) { //recorro los componentes
+    x[i].style.display = "flex"; //muestro los componentes display
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
 }
-function tran() {//funcion para mostrar los componentes transistores
-  borra();//borro los componentes
+
+function tran() { //funcion para mostrar los componentes transistores
+  borra(); //borro los componentes
   var x, i;
-  x = document.querySelectorAll(".tra");//obtengo los componentes transistores
-  for (i = 0; i < x.length; i++) {//recorro los componentes
-    x[i].style.display = "flex";//muestro los componentes transistores
+  x = document.querySelectorAll(".tra"); //obtengo los componentes transistores
+  for (i = 0; i < x.length; i++) { //recorro los componentes
+    x[i].style.display = "flex"; //muestro los componentes transistores
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
 }
-function serv() {//funcion para mostrar los componentes transistores
-  borra();//borro los componentes
+
+function serv() { //funcion para mostrar los componentes transistores
+  borra(); //borro los componentes
   var x, i;
-  x = document.querySelectorAll(".ser");//obtengo los componentes servidores
-  for (i = 0; i < x.length; i++) {//recorro los componentes
-    x[i].style.display = "flex";//muestro los componentes servidores
+  x = document.querySelectorAll(".ser"); //obtengo los componentes servidores
+  for (i = 0; i < x.length; i++) { //recorro los componentes
+    x[i].style.display = "flex"; //muestro los componentes servidores
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
 }
-function gral() {//funcion para mostrar los componentes transistores
-  borra();//borro los componentes
+
+function gral() { //funcion para mostrar los componentes transistores
+  borra(); //borro los componentes
   var x, i;
-  x = document.querySelectorAll(".gra");//obtengo los componentes generales
-  for (i = 0; i < x.length; i++) {//recorro los componentes
-    x[i].style.display = "flex";//muestro los componentes generales
+  x = document.querySelectorAll(".gra"); //obtengo los componentes generales
+  for (i = 0; i < x.length; i++) { //recorro los componentes
+    x[i].style.display = "flex"; //muestro los componentes generales
   }
   const navMenu2 = document.querySelector(".category_list"); //obtengo el elemento nav-menu
   navMenu2.classList.toggle("category_list__visible");
+}
+//-----------------------------------------------------------------------------------
+//funcion de salida de pagina
+//-----------------------------------------------------------------------------------
+function sale() {
+  window.close()
+}
+
+function voy_index() {
+  window.location("https://mariogonzalezispc.github.io/g5-a3g5_2/index.html")
 }
--- js/validacion.js
+++ js/validacion.js
@@ -3,17 +3,17 @@
 //-----------------------------------------------------------------------------------
 //puesta en fecha actual del input data
 //-----------------------------------------------------------------------------------
-window.onload = function(){
+window.onload = function () {
     var fecha = new Date(); //Fecha actual
-    var mes = fecha.getMonth()+1; //obteniendo mes
+    var mes = fecha.getMonth() + 1; //obteniendo mes
     var dia = fecha.getDate(); //obteniendo dia
     var ano = fecha.getFullYear(); //obteniendo año
-    if(dia<10)
-      dia='0'+dia; //agrega cero si el menor de 10
-    if(mes<10)
-      mes='0'+mes //agrega cero si el menor de 10
-    document.getElementById('fecha_n').value=ano+"-"+mes+"-"+dia;
-  }
+    if (dia < 10)
+        dia = '0' + dia; //agrega cero si el menor de 10
+    if (mes < 10)
+        mes = '0' + mes //agrega cero si el menor de 10
+    document.getElementById('fecha_n').value = ano + "-" + mes + "-" + dia;
+}
 //-----------------------------------------------------------------------------------
 //Validacion del formulario registro
 //-----------------------------------------------------------------------------------
@@ -24,44 +24,44 @@ document.addEventListener("DOMContentLoaded", function () {
 function validarRegistro(evento) {
     evento.preventDefault();
 
-    var name = document.getElementById('nombres').value;//capturo value del input nombre
-    var ape = document.getElementById('apellidos').value;//capturo value del input apellido
-    var dni = document.getElementById('dni').value;//capturo value del input dni
-    var fechaN = document.getElementById('fecha_n').value;//capturo value del input fecha nacimiento
-    var correo = document.getElementById('email').value;//capturo value del input correo  
-    var clave1 = document.getElementById('clave1').value;//capturo value del input clave1
-    var clave2 = document.getElementById('clave2').value;//capturo value del input clave2
+    var name = document.getElementById('nombres').value; //capturo value del input nombre
+    var ape = document.getElementById('apellidos').value; //capturo value del input apellido
+    var dni = document.getElementById('dni').value; //capturo value del input dni
+    var fechaN = document.getElementById('fecha_n').value; //capturo value del input fecha nacimiento
+    var correo = document.getElementById('email').value; //capturo value del input correo  
+    var clave1 = document.getElementById('clave1').value; //capturo value del input clave1
+    var clave2 = document.getElementById('clave2').value; //capturo value del input clave2
 
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;//creo una variable para la expresion regular
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; //creo una variable para la expresion regular
 
-    if (name.length < 3) {//si el nombre es menor a 3
-        if (name.length == 0) {//si el nombre es igual a 0
-            swal('No has escrito nada en el nombre','','error');//muestro un mensaje
-            return;//salgo del evento submit y lo interrumpo por completo 
-        } else {//  
-            swal('El nombre debe tener al menos tres letras','','error');//muestro un mensaje
-            return;//salgo del evento submit y lo interrumpo por completo 
+    if (name.length < 3) { //si el nombre es menor a 3
+        if (name.length == 0) { //si el nombre es igual a 0
+            swal('No has escrito nada en el nombre', '', 'error'); //muestro un mensaje
+            return; //salgo del evento submit y lo interrumpo por completo 
+        } else { //  
+            swal('El nombre debe tener al menos tres letras', '', 'error'); //muestro un mensaje
+            return; //salgo del evento submit y lo interrumpo por completo 
         }
     }
     if (ape.length < 3) {
         if (ape.length == 0) {
-            swal('No has escrito nada en el apellido','','error');
-            return;//salgo del evento submit y lo interrumpo por completo
+            swal('No has escrito nada en el apellido', '', 'error');
+            return; //salgo del evento submit y lo interrumpo por completo
         } else {
-            swal('El apellido debe tener al menos tres letras','','error');
-            return;//salgo del evento submit y lo interrumpo por completo 
+            swal('El apellido debe tener al menos tres letras', '', 'error');
+            return; //salgo del evento submit y lo interrumpo por completo 
         }
     }
     if (dni.length < 7) {
         if (dni.length == 0) {
-            swal('No has escrito tu DNI','','error');
-            return;//salgo del evento submit y lo interrumpo por completo
+            swal('No has escrito tu DNI', '', 'error');
+            return; //salgo del evento submit y lo interrumpo por completo
         } else {
-            swal('El DNI debe tener al menos siete numeros','','error');
-            return;//salgo del evento submit y lo interrumpo por completo
+            swal('El DNI debe tener al menos siete numeros', '', 'error');
+            return; //salgo del evento submit y lo interrumpo por completo
         }
     }
-    /*------------------------------------------------------------------------------------*/
+    /*------------------------------------------------------------------------------*/
     var hoy = new Date();
     var cumpleanos = new Date(fechaN);
     var edad = hoy.getFullYear() - cumpleanos.getFullYear();
@@ -70,30 +70,31 @@ function validarRegistro(evento) {
         edad--;
     }
     if (edad <= 18) {
-        swal("Eres menor de edad :( ",'','error');
+        swal("Eres menor de edad :( ", '', 'error');
         return;
     }
-    /*------------------------------------------------------------------------------------*/
+    /*------------------------------------------------------------------------------*/
     if (!regexEmail.test(email.value)) {
-        swal('Formato de correo incorrecto','','error');
+        swal('Formato de correo incorrecto', '', 'error');
         return;
     }
 
     if (clave1.length < 5) {
         if (clave1.length == 0) {
-            swal('No cargo ninguna contraseña','','error');
+            swal('No cargo ninguna contraseña', '', 'error');
             return;
         } else {
-            swal('La clave debe tener al menos 6 caracteres','','error');
+            swal('La clave debe tener al menos 6 caracteres', '', 'error');
             return;
         }
     }
     if (clave2.length < 6 || clave2 != clave1) {
-        swal('Repita las dos contraseñas no estan iguales','','error');
+        swal('Repita las dos contraseñas no estan iguales', '', 'error');
         return;
     }
-    swal('Sus datos se cargaran ahora','','success');
+    //swal('se ha registrado con exito','','success');
     this.submit();
+
 }
 //-----------------------------------------------------------------------------------
 //Validacion del formulario contacto
@@ -104,36 +105,40 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function validarContacto(evento) {
     evento.preventDefault();
-    var name = document.getElementById('nombres').value;
-    var correo = document.getElementById('email').value;
-    var mensaje = document.getElementById('mensaje').value;
+    var name = document.getElementById('nombres').value; //capturo value del input nombre
+    var correo = document.getElementById('email').value; //capturo value del input correo
+    var mensaje = document.getElementById('mensaje').value; //capturo value del input mensaje
 
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     if (name.length < 3) {
         if (name.length == 0) {
-            swal('No has escrito nada en el nombre','','error');
+            swal('No has escrito nada en el nombre', '', 'error');
             return;
         } else {
-            swal('El nombre debe tener al menos tres letras','','error');
+            swal('El nombre debe tener al menos tres letras', '', 'error');
             return;
         }
     }
-    if(!regexEmail.test(email.value)){
-        swal('Formato de correo incorrecto','','error');
+
+    if (!regexEmail.test(email.value)) {//si el email no cumple la expresion regular
+        swal('Formato de correo incorrecto', '', 'error');
         return;
     }
+
     if (mensaje.length < 5) {
-        if (mensaje.length == 0) {
-            swal('No has escrito nada en el mensaje','','error');
-            return;
+        if (mensaje.length == 0) {//si el mensaje es igual a 0
+            swal('No has escrito nada en el mensaje', '', 'error');
+            return;//salgo del evento submit y lo interrumpo por completo
         } else {
-            swal('El mensaje debe tener al menos cinco letras','','error');
-            return;
+            swal('El mensaje debe tener al menos cinco letras', '', 'error');
+            return;//salgo del evento submit y lo interrumpo por completo
         }
     }
-    this.submit();
-}
+    this.submit();//si todo esta bien envio el formulario
+    mi_Envio('contacto1.html', '_blank');//llamo a la funcion mi_Envio
+    voy_index(); //llamo a la funcion voy_index para cargar la pagina index
+ }
 
 //-----------------------------------------------------------------------------------
 //Validacion del formulario login
@@ -149,16 +154,16 @@ function validarLogin(evento) {
 
     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if(!regexEmail.test(email.value)){
-        swal('Formato de correo incorrecto','','error');
+    if (!regexEmail.test(email.value)) {
+        swal('Formato de correo incorrecto', '', 'error');
         return;
     }
     if (clave1.length < 6) {
         if (clave1.length == 0) {
-            swal('No cargo ninguna contraseña','','error');
+            swal('No cargo ninguna contraseña', '', 'error');
             return;
         } else {
-            swal('La contraseña debe tener al menos seis letras','','error');
+            swal('La contraseña debe tener al menos seis letras', '', 'error');
             return;
         }
     }
